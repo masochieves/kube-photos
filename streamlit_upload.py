62413c308197aa34566cea489d9bc97b38836a54
pseuds <xinweiching@gmail.com>
2024-12-21 18:45:57 +0800

    Completed gallery page

--- streamlit_upload.py
+++ streamlit_upload.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from io import BytesIO
 import os 
-import pandas as pd
+from math import ceil
 
 # Path to image files
 # os.chdir("/home/chief/kube-photos/")
@@ -15,21 +15,23 @@ class GalleryPage():
         self.is_display_mode = True     # false if slideshow, true if selected display
         self.selected_image = "-"       # path of selected image
         self.page_no = 1                # gallery page
-        self.initialise_page()
+        # self.initialise_page()
 
     def initialise_page(self):
         st.title("Gallery")
+
+        row_size = 4
+        batch_size = 8
         img_dir = "./images/"
-        num_batches = get_total_images()
-        print("num_batches", num_batches)
+        num_batches = ceil(get_total_images()/batch_size)
         files = os.listdir(img_dir)
 
         # Show current display status
         self.current_state_text = st.empty()
-        self.current_state_text.markdown("Current display mode:" 
-                                         + (f"Displaying {self.selected_image}" if self.is_display_mode else "Slideshow"))
+        self.current_state_text.markdown("Current display mode: " 
+                                         + (f"Displaying {st.session_state['displayed_image']}" if st.session_state['is_display_mode'] else "Slideshow"))
         self.current_img = st.empty()
-        self.current_img.markdown("Selected image:"+self.selected_image)
+        self.current_img.markdown("Selected image: "+st.session_state['selected_image'])
 
         # Initialise control buttons
         controls = st.columns(4) # display image, slideshow mode
@@ -43,8 +45,6 @@ class GalleryPage():
             self.page_no = st.selectbox("Page", range(1, num_batches+1))
 
         # Show gallery of images
-        row_size = 4
-        batch_size = 20
         batch_imgs = files[(self.page_no-1)*batch_size:self.page_no*batch_size]
 
         grid = st.columns(row_size)
@@ -53,30 +53,26 @@ class GalleryPage():
         for img in batch_imgs:
             with grid[col]:
                 st.image(img_dir+img, caption=img)
-                _ = st.button("Select", on_click=lambda:self.on_click_select_img(img))
+                _ = st.button("Select", key=f"button_{img}", on_click=lambda:self.on_click_select_img(img))
             col = (col+1) % row_size
 
     # Button functions
     def on_click_mode_buttons(self, clicked_button):
-        show_str = "Current display mode:"
+        show_str = "Current display mode: "
         # if clicked_button == 0, clicked button is 'display selected'
         if clicked_button == 0:
-            self.selected_image = self.get_selected_img()
-            self.is_display_mode = True
+            st.session_state['is_display_mode'] = True
+            st.session_state['displayed_image'] = st.session_state['selected_image']
         # if clicked_button == 1, clicked button is 'slideshow'
         elif clicked_button == 1:
-            self.is_display_mode = False
+            st.session_state['is_display_mode'] = False
 
         self.current_state_text.markdown(
-            show_str + (f"Displaying {self.selected_image}" if self.is_display_mode else "Slideshow"))
+            show_str + (f"Displaying {st.session_state['displayed_image']} " if st.session_state['is_display_mode'] else "Slideshow"))
         
     def on_click_select_img(self, img_name):
-        self.selected_image = img_name
-        self.current_img.markdown("Selected image:"+self.selected_image)
-
-
-
-
+        st.session_state['selected_image'] = img_name
+        self.current_img.markdown("Selected image: "+st.session_state['selected_image'])
 
 def upload_page():
     st.title("Upload Images")
@@ -105,6 +101,15 @@ def get_total_images():
 
 
 if __name__ == "__main__":
+    # Session states
+    if 'is_display_mode' not in st.session_state:
+        st.session_state['is_display_mode'] = True
+    if 'selected_image' not in st.session_state:
+        st.session_state['selected_image'] = "-"
+    if 'displayed_image' not in st.session_state:
+        st.session_state['displayed_image'] = "-"
+
     # Have pages in a navigation bar
-    nav = st.navigation([st.Page(GalleryPage(), title="Display Image"), st.Page(upload_page, title="Upload Image")])
+    gallery_page = GalleryPage().initialise_page
+    nav = st.navigation([st.Page(gallery_page, title="Display Image"), st.Page(upload_page, title="Upload Image")])
     nav.run()
